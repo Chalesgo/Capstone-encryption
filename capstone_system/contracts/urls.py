994a8c6a86a1b2c78e399a53fdea646739feca27
Griffeminie <103305060+Griffeminie@users.noreply.github.com>
2026-04-07 01:12:10 +0800

    Add files via upload

---
+++ capstone_system/contracts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.contract_list, name='contract_list'),
+    path('upload/', views.upload_contract, name='upload_contract'),
+]
