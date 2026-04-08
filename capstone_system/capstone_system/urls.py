7fb3fc9a08a15e52e8ffbf1e96da433c0ea9a949
Griffeminie <103305060+Griffeminie@users.noreply.github.com>
2026-04-09 05:01:15 +0800

    Add files via upload

--- capstone_system/capstone_system/urls.py
+++ capstone_system/capstone_system/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contracts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
 
 if settings.DEBUG:
